c986a7b4b786bd4711e0e4b1471cdff77f8bdd18
bamandel <bamandel@gmail.com>
2020-10-01 23:40:59 -0400

    Revert version updates and add first component

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.css']
+})
+export class SquareComponent {
+
+}
