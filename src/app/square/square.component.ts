6fdfcdf6925fc57ceaa78b3b7e806679dd48d20c
bamandel <bamandel@gmail.com>
2020-10-02 14:53:51 -0400

    Fixed styling for tic tac toe buttons

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   template: `
   <button>{{ value }}</button>
   `,
-  styles: []
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O'
